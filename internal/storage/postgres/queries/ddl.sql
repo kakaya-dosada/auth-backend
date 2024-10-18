61247622cca72d801e4eb1915d4be0b5618f6361
DenisOzindzheDev <demaxdeniz.do@gmail.com>
2024-10-18 15:42:45 +0300

    database ddl

---
+++ internal/storage/postgres/queries/ddl.sql
@@ -0,0 +1,21 @@
+-- Таблица Role
+CREATE TABLE IF NOT EXISTS roles (
+    id BIGINT PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+
+-- Таблица User
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT PRIMARY KEY,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    role_id INT NOT NULL DEFAULT 3,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+     FOREIGN KEY (role_id) REFERENCES public.roles(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
